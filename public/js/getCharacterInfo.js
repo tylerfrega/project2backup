2115eab247b3e6e05d4e9d537280f417df2ba272
Tyler J. Frega <tyfrega@gmail.com>
2017-09-19 21:38:51 -0400

    character construstor function working

--- public/js/getCharacterInfo.js
+++ public/js/getCharacterInfo.js
@@ -1,6 +1,16 @@
 $(document).ready(function(){
+
+makeCharacterButtons();
+$(document).on('click', '.characterSelector', selectCharacter );
+//end of ready function
+})
+
+var characters;
+
+
+function makeCharacterButtons(){
     var characterHolder = $('#characterDisplay')
-    var characters;
+    
     //gets character data
     $.get('/api/character', function(data){
         characters = data
@@ -8,23 +18,33 @@ $(document).ready(function(){
     }).then(function(){
 
     //makes character buttons
-    characters.forEach(function(character) {
+    characters.forEach(function(character, index) {
         console.log(character.characterName)
-        characterHolder.append(`<button class="characterSelector">${character.characterName}</button>`);
+        characterHolder.append(`<button class="characterSelector" data-value= "${index}">${character.characterName}</button>`);
     });
 
-    $('.characterSelector').on('click', selectCharacter);
 
 });
-
-
-function selectCharacter(){
-    
 }
 
 
-//end of ready function
-})
+function Character(characterName, characterClass, hp, ap, de, ) {
+    this.characterName = characterName;
+    this.characterClass = characterClass;
+    this.hp = hp;
+    this.ap = ap;
+    this.de = de;
+}
 
 
-
+function selectCharacter(){
+     var selectedCharacter = $(this).attr('data-value');
+    
+    var sessionCharacter = new Character(characters[selectedCharacter].characterName, 
+                                         characters[selectedCharacter].class,
+                                         characters[selectedCharacter].hp,
+                                         characters[selectedCharacter].ap,
+                                         characters[selectedCharacter].de);
+    console.log(sessionCharacter);
+}
+    
